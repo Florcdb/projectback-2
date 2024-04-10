bf4c72e961f183dde7840114a5f8531d4d7e78bd
Florcdb <florcordoba51@gmail.com>
2024-04-10 15:19:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+PORT = 3000
+
+
+app.get('/', (req, res) => (res.send('Proyecto BACK')))
+ 
+app.listen(PORT, () => {
+    console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
+})
