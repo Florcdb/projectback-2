49da85cca14967f54c1575c9acac3ab7644ea51d
Florcdb <florcordoba51@gmail.com>
2024-04-10 15:42:12 +0200

    create a new Service

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-PORT = 3000
+require('dotenv').config()
 
 
-app.get('/', (req, res) => (res.send('Proyecto BACK')))
+
+
+PORT = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => (res.send('Proyecto BACK')))
  
 app.listen(PORT, () => {
     console.log(`Express está escuchando en el puerto http://localhost:${PORT}`)
